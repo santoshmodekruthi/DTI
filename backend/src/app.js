a0ad6b0bc866b03cd088d3d52566bbb78fd53031
santoshmodekruthi <santoshmodekruthi4@gmail.com>
2025-12-29 12:48:11 +0530

    changed

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser"
 import { ApiError } from "./utils/ApiError.js"
 const app = express()
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://dti-tan.vercel.app",
     credentials: true,
 }))
 app.use(express.json())
